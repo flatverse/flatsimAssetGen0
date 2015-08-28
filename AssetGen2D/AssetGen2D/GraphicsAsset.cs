675d28e2c627feab4900d1c8f061384caacc8b6c
codyThompson <cody.thompson22@gmail.com>
2015-08-27 18:29:57 -0700

    Created a GraphicsAsset class that basically just stores the DrawingVisual and adds some helper methods so that an asset can be passed from function to function.

---
+++ AssetGen2D/AssetGen2D/GraphicsAsset.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace flatsim.AssetGen2D
+{
+    public class GraphicsAsset
+    {
+        DrawingVisual drawingVisual;
+
+        public GraphicsAsset()
+        {
+            drawingVisual = new DrawingVisual();
+        }
+
+        public DrawingContext renderOpen()
+        {
+            return drawingVisual.RenderOpen();
+        }
+
+        public void renderClose(DrawingContext dc)
+        {
+            dc.Close();
+        }
+
+        public void saveAsPng(int width, int height, int dpiX, int dpiY, string outputPath)
+        {
+            RenderTargetBitmap bmp = new RenderTargetBitmap(width, height, dpiX, dpiY, PixelFormats.Pbgra32);
+            bmp.Render(drawingVisual);
+
+            PngBitmapEncoder png = new PngBitmapEncoder();
+            png.Frames.Add(BitmapFrame.Create(bmp));
+            using (Stream stm = File.Create(outputPath))
+            {
+                png.Save(stm);
+            }
+        }
+    }
+}
